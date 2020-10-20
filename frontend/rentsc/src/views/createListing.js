33a266cf5e061f85e97116993fe32c207a2d4459
Stanley Tran <stanleyltran@gmail.com>
2020-10-20 15:32:33 -0700

    Form with multiple fields successfully takes input

--- frontend/rentsc/src/views/createListing.js
+++ frontend/rentsc/src/views/createListing.js
@@ -1,10 +1,47 @@
 import React from 'react';
 
+class ListingFields extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            address: "",
+            price: ""
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('Address: ' + this.state.address + ", Price: " + this.state.price);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Address:
+                    <input type="text" name="address" value={this.state.address} onChange={this.handleChange} />
+                </label>
+                <br></br>
+                <label>
+                    Price:
+                    <input type="text" name="price" value={this.state.price} onChange={this.handleChange} />
+                </label>
+                <br></br>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
 function createListing() {  
     return(
-        <div>
-            <h1>Listing Page</h1>
-        </div>
+        <ListingFields />
     );
 }
 

5b192f0f867b08d989c82a78b0fc4f65bf9f0d89
Michael <Michael@kgovindarals-MacBook-Pro.local>
2020-11-24 21:07:32 -0800

    Resolve Pull Conflicts

--- frontend/rentsc/src/App.js
+++ frontend/rentsc/src/App.js
@@ -20,35 +20,15 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-<<<<<<< HEAD
-        <Route exact path="/listings" component={listing} />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/create-listing" component={createListing} />
-        <Route exact path="/signUp1" component={Signup1} />
-        <Route exact path="/messages" component={Messages} />
-||||||| merged common ancestors
-        <Route exact path="/listings" component={listing} />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/create-listing" component={createListing} />
-        <Route exact path="/signUp1" component={Signup1} />
-=======
->>>>>>> 6908dbadd7775293446fc241e5e21ac1fa49184c
         <AuthProvider>
           <PrivateRoute exact path="/userprofile" component={UserProfile} />
           <Route path="/signup" component={Signup} />
           <Route path="/login" component={Login} />
+          <Route path="/messages" component={Messages} />
           <Route exact path="/create-listing" component={createListing} />
           <Route exact path="/listings" component={listing} />
           <Route exact path="/" component={Home} />
         </AuthProvider>
-<<<<<<< HEAD
-        <Route exact path="/signUp1" component={Signup} />
-||||||| merged common ancestors
-        <Route exact path="/signUp1" component={Signup} />
-
-=======
-
->>>>>>> 6908dbadd7775293446fc241e5e21ac1fa49184c
         <Route component={PageNotFound} />
       </Switch>
     </BrowserRouter>

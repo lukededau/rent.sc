ffc6ae05bb874eb948245b322db4556a84a293bb
Michael <Michael@kgovindarals-MacBook-Pro.local>
2020-12-06 20:45:22 -0800

    Added more unit tests/minor code cleanup

--- frontend/rentsc/src/views/listing.js
+++ frontend/rentsc/src/views/listing.js
@@ -1,32 +1,19 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
 import Home from './Home.js'
 import NavigationBar from '../Components/navbar.js'
 import ListingList from '../Components/listingList.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-/*
-<Container fluid
-                    style={{paddingTop:"60px"}}
-                >
-                    <Row>
-                        <Col style={{overflowY: "scroll"}}><ListingList/></Col>  
-                        <Col style={{position: "fixed !important", top: "0px !important"}}><Home /></Col>
-                    </Row>
-</Container>
-*/
-
 class listing extends React.Component {
     render() {
-        //document.body.style.overflow = 'hidden'; // disable pagewise scroll
         return (
             <div>
                 <NavigationBar></NavigationBar>
-                <div style={{ paddingTop: "60px" }}>
-                    <div style={{ width: "50%", display: "inline-block" }}>
+                <div id="listingContainer"style={{ paddingTop: "60px" }}>
+                    <div id="listingList" style={{ width: "50%", display: "inline-block" }}>
                         <ListingList />
                     </div>
-                    <div style={{ top: "60px", position: "fixed", width: "50%", display: "inline-block" }}>
+                    <div id="listingMap" style={{ top: "60px", position: "fixed", width: "50%", display: "inline-block" }}>
                         <Home />
                     </div>
 

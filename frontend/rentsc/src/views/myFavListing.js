9fbc22306743e8a5b5707cddadb913036091f8a4
myhpham <57925032+myhpham@users.noreply.github.com>
2020-12-08 20:02:11 -0800

    mylistings + myfavorites not accessible unless user is logged in

--- frontend/rentsc/src/views/myFavListing.js
+++ frontend/rentsc/src/views/myFavListing.js
@@ -2,19 +2,30 @@ import React from 'react';
 import NavigationBar from '../Components/navbar.js'
 import MyFavListingList from '../Components/MyFavlistingList.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import firebase from '../firebase'
+import { Redirect } from 'react-router-dom'
 
 class listing extends React.Component {
-    render() {
+    checkLogin() {
+        var login
+        if(firebase.auth().currentUser != null) {
+            login = true
+        } else {
+            login = false
+        }
+        return login
+    }
 
+    render() {
         return (
             <div>
                 <NavigationBar></NavigationBar>
 
-                <div style={{ paddingTop: "60px" }}>
+                {this.checkLogin() ? <div style={{ paddingTop: "60px" }}>
                     <div style={{ width: "50%", display: "inline-block" }}>
                         <MyFavListingList />
                     </div>
-                </div>
+                </div> : <Redirect to='/login'/>}
 
             </div>
         );
